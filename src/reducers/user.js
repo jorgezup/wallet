869f97b789516df5c0a239211ebc302b5b185e59
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-03-04 23:05:28 -0400

    configurated redux files, reducers and store

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { USER } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+function userReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case USER:
+    return { ...state, email: action.payload };
+  default:
+    return state;
+  }
+}
+
+export default userReducer;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { WALLET } from '../actions';
+
+const INITIAL_STATE = {
+  data: '',
+};
+
+function walletReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case WALLET:
+    return { ...state, data: action.payload };
+  default:
+    return state;
+  }
+}
+
+export default walletReducer;
