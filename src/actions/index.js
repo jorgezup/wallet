b7b125f0d385d4cb0e52c2824b68a5fde32ebae5
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-03-04 23:06:27 -0400

    configurated actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
 // Coloque aqui suas actions
+export const USER = 'USER';
+export const WALLET = 'WALLET';
+
+export const userData = (data) => ({
+  type: 'USER',
+  payload: data,
+});
+
+export const walletData = (data) => ({
+  type: 'WALLET',
+  payload: data,
+});
