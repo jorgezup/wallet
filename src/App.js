42c0ff2e9ce9a9843c98ff4a075ba8fef72d3c28
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-03-04 23:05:58 -0400

    configurated routes and store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route path="/carteira" render={ (props) => <Wallet { ...props } /> } />
+        <Route path="/" exact render={ (props) => <Login { ...props } /> } />
+      </Switch>
+    );
+  }
 }
 
 export default App;
