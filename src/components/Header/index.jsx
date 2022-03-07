ec10cf4b59faeb814bb3eafa7eab84b87327ea47
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-03-06 23:36:38 -0400

    added function to sum total expenses

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,8 +3,16 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+  totalExpense = (expenses) => expenses.reduce((acc, expense) => {
+    const filteredCurrency = expense.exchangeRates[expense.currency];
+    const askValue = filteredCurrency.ask;
+    const calculatedValue = Number(expense.value) * Number(askValue);
+    acc += calculatedValue;
+    return acc;
+  }, 0)
+
   render() {
-    const { email } = this.props;
+    const { email, expenses } = this.props;
     return (
       <header>
         <div>
@@ -13,7 +21,7 @@ class Header extends Component {
         </div>
         <div>
           <span>Despesa total: </span>
-          <span data-testid="total-field">0</span>
+          <span data-testid="total-field">{this.totalExpense(expenses)?.toFixed(2)}</span>
         </div>
         <div>
           <span data-testid="header-currency-field">BRL</span>
@@ -26,10 +34,12 @@ class Header extends Component {
 
 Header.propTypes = {
   email: PropTypes.string.isRequired,
+  expenses: PropTypes.arrayOf(Object).isRequired,
 };
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  expenses: state.wallet.expenses,
 });
 
 export default connect(mapStateToProps, null)(Header);
